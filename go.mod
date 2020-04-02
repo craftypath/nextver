45a027c99cfede5d0b18fc7c1f788dcd6b21920d
René Galle <rene.galle@codecentric.de>
2020-04-02 14:12:09 +0200

    remove semver lib

--- go.mod
+++ go.mod
@@ -3,6 +3,5 @@ module verit
 go 1.14
 
 require (
-	github.com/Masterminds/semver/v3 v3.0.3
 	github.com/magefile/mage v1.9.0
 )
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@ package version
 
 import (
     "fmt"
-    "github.com/Masterminds/semver/v3"
+    "strconv"
     "strings"
 )
 
@@ -11,28 +11,17 @@ const positionMinor = 1
 const positionPatch = 2
 
 func Next(current string, patternNext string) (string, error) {
-    currentVersion, err := semver.StrictNewVersion(current)
+    currentMajor, currentMinor, currentPatch, _, err := components(current)
     if err != nil {
         return "", err
     }
 
-    versionNext, err := semver.StrictNewVersion(strings.ReplaceAll(patternNext, "x", "0"))
+    nextMajor, nextMinor, nextPatch, nextExtension, err := components(patternNext)
     if err != nil {
         return "", err
     }
 
-    patternNextCore, patternNextExtension := splitVersionExtension(patternNext)
-
-    // set and maybe increment new version
-    nextMajor := versionNext.Major()
-    nextMinor := versionNext.Minor()
-    nextPatch := versionNext.Patch()
-
-    currentMajor := currentVersion.Major()
-    currentMinor := currentVersion.Minor()
-    currentPatch := currentVersion.Patch()
-
-    positionX, err := findX(patternNextCore)
+    positionX, err := findX(patternNext)
     if err != nil {
         return "", err
     }
@@ -52,28 +41,43 @@ func Next(current string, patternNext string) (string, error) {
         }
     }
 
-    var result string
-    nextVersion, err := semver.StrictNewVersion(fmt.Sprintf("%v.%v.%v%v", nextMajor, nextMinor, nextPatch, patternNextExtension))
-    if err == nil {
-        result = nextVersion.String()
-    }
-    return result, err
+    return fmt.Sprintf("%v.%v.%v%v", nextMajor, nextMinor, nextPatch, nextExtension), err
 }
 
-func splitVersionExtension(version string) (string, string) {
-    versionCore := version
-    var versionExtension string
-    if indexExtension := strings.IndexAny(version, "+-"); indexExtension != -1 {
-        versionCore = version[:indexExtension]
-        versionExtension = version[indexExtension:]
+func components(version string) (uint, uint, uint, string, error) {
+    core, extension := getCoreAndExtension(version)
+    core = strings.ReplaceAll(core, "x", "0")
+    split := strings.Split(core, ".")
+    if len(split) != 3 {
+        return 0, 0, 0, "", fmt.Errorf("version must be <major>.<minor>.<patch>, got %v", version)
     }
-    return versionCore, versionExtension
+    major, err := strconv.Atoi(split[0])
+    if err != nil {
+        return 0, 0, 0, "", err
+    }
+    minor, err := strconv.Atoi(split[1])
+    if err != nil {
+        return 0, 0, 0, "", err
+    }
+    patch, err := strconv.Atoi(split[2])
+    if err != nil {
+        return 0, 0, 0, "", err
+    }
+    if major < 0 || minor < 0 || patch < 0 {
+        return 0, 0, 0, "", fmt.Errorf("version must only contain positive numbers got %v", version)
+    }
+    return uint(major), uint(minor), uint(patch), extension, nil
 }
 
+// returns 0 if major is x
+// returns 1 if minor is x
+// returns 2 if patch is x
+// returns an error if there is more than one x in major, minor, and patch combined
 func findX(version string) (int, error) {
+    versionCore, _ := getCoreAndExtension(version)
     positionX := -1
     var countX int
-    for i, s := range strings.Split(version, ".") {
+    for i, s := range strings.Split(versionCore, ".") {
         if s == "x" {
             positionX = i
             countX++
@@ -83,4 +87,14 @@ func findX(version string) (int, error) {
         }
     }
     return positionX, nil
+}
+
+func getCoreAndExtension(version string) (string, string) {
+    versionCore := version
+    var versionExtension string
+    if indexExtension := strings.IndexAny(version, "+-"); indexExtension != -1 {
+        versionCore = version[:indexExtension]
+        versionExtension = version[indexExtension:]
+    }
+    return versionCore, versionExtension
 }
